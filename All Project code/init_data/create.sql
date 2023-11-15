b78d4a22615bddf4f2114235871b36029c766540
Jerry Li <jli20031111@gmail.com>
2023-11-15 15:08:21 -0700

    added reports table

--- All Project code/init_data/create.sql
+++ All Project code/init_data/create.sql
@@ -3,3 +3,12 @@ CREATE table users (
     username VARCHAR(50) PRIMARY KEY,
     password CHAR(60) NOT NULL
 );
+
+DROP TABLE IF EXISTS reports CASCADE;
+CREATE TABLE reports(
+    report_id INT PRIMARY KEY,
+    observations TEXT NOT NULL,
+    date DATE NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users (username)   
+);
