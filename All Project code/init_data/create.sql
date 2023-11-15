f92deba003b00bbcc10c1de2a2c4d9fe321954e2
Jerry Li <jli20031111@gmail.com>
2023-11-15 16:39:33 -0700

    fixed register return none

--- All Project code/init_data/create.sql
+++ All Project code/init_data/create.sql
@@ -4,11 +4,11 @@ CREATE table users (
     password CHAR(60) NOT NULL
 );
 
-DROP TABLE IF EXISTS reports CASCADE;
+/*DROP TABLE IF EXISTS reports CASCADE;
 CREATE TABLE reports(
     report_id INT PRIMARY KEY,
     observations TEXT NOT NULL,
     date DATE NOT NULL,
     username VARCHAR(50) NOT NULL,
     FOREIGN KEY (username) REFERENCES users (username)   
-);
+);*/
